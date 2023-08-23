aba0da3ba2fbd25accb0d6ef597e86fbf27462f8
Kyu Hyong Park <kjp5774@psu.edu>
2023-08-23 15:52:24 -0400

    fix paths

--- boolmore/config.py
+++ boolmore/config.py
@@ -1,6 +1,6 @@
 # location of the starting model or the model to make a detailed report
 MODEL = None
-# MODEL = 'networkmutation/generated_models/20230512_7790_gen125_mod.txt'
+# MODEL = 'BoolMoRe/generated_models/20230512_7790_gen125_mod.txt'
 
 # name of the key to use in the data bank
 RUN_TYPE = 'ABA_normal'
@@ -25,8 +25,8 @@ parameters = {
 
 data_bank = {
     'ABA_normal':{
-        'data': 'networkmutation/data/data_20230426.tsv',
-        'base': 'networkmutation/baseline_models/ABA_GA_base_A_20230501.txt',
+        'data': 'BoolMoRe/data/data_20230426.tsv',
+        'base': 'BoolMoRe/baseline_models/ABA_GA_base_A_20230501.txt',
         'default_sources': {'ABA':0},
         ### constraints for the typical run 20230815
         'constraints': {'fixed': {'Ca2_ATPase', 'Ca2c', 'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
@@ -45,8 +45,8 @@ data_bank = {
         },
 
     'ABA_normal_more_edges':{
-        'data': 'networkmutation/data/data_20230426.tsv',
-        'base': 'networkmutation/baseline_models/ABA_GA_base_A_20230501.txt',
+        'data': 'BoolMoRe/data/data_20230426.tsv',
+        'base': 'BoolMoRe/baseline_models/ABA_GA_base_A_20230501.txt',
         'default_sources': {'ABA':0},
         ### constraints for the 'more edges' version 20230815 - allow change of Ca2c, move to regulate
         'constraints' : {'fixed': {'Ca2_ATPase', 'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
@@ -71,8 +71,8 @@ data_bank = {
         },
 
     'ABA_osc':{
-        'data': 'networkmutation/data/data_osc_20230426.tsv',
-        'base': 'networkmutation/baseline_models/ABA_GA_base_B_20230407.txt',
+        'data': 'BoolMoRe/data/data_osc_20230426.tsv',
+        'base': 'BoolMoRe/baseline_models/ABA_GA_base_B_20230407.txt',
         'default_sources': {'ABA':0},
         ### constraints for the typical run of the ca_osc model 20230815
         'constraints': {'fixed': {'Ca2osc', 'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
@@ -91,8 +91,8 @@ data_bank = {
         },
     
     'ABA_osc_more_edges':{
-        'data': 'networkmutation/data/data_osc_20230426.tsv',
-        'base': 'networkmutation/baseline_models/ABA_GA_base_B_20230407.txt',
+        'data': 'BoolMoRe/data/data_osc_20230426.tsv',
+        'base': 'BoolMoRe/baseline_models/ABA_GA_base_B_20230407.txt',
         'default_sources': {'ABA':0},
         ### constraints for the 'more edges' version of ca_osc model 20230815 - allow modification of Ca2osc, move to regulate
         'constraints': {'fixed': {'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
@@ -117,133 +117,20 @@ data_bank = {
         },
 
     'ABA_2006_Li':{
-        'data': 'networkmutation/data/data_Li_20230811.tsv',
-        'base': 'networkmutation/baseline_models/ABA_2006_Li.txt',
+        'data': 'BoolMoRe/data/data_Li_20230811.tsv',
+        'base': 'BoolMoRe/baseline_models/ABA_2006_Li.txt',
         'default_sources': {'ABA':0},
         'constraints': {},
         'edge_pool': [],
         },
     
     'ABA_2018_Waidyarathne':{
-        'data': 'networkmutation/data/data_Waidyarathne_20230811.tsv',
-        'base': 'networkmutation/baseline_models/ABA_2018_Waidyarathne.txt',
+        'data': 'BoolMoRe/data/data_Waidyarathne_20230811.tsv',
+        'base': 'BoolMoRe/baseline_models/ABA_2018_Waidyarathne.txt',
         'default_sources': {'ABA':0},
         'constraints': {},
         'edge_pool': [],
         },
     }
-
-# STARTING_ID = 1
-# STARTING_GEN = 1
-# ITERATIONS = 100
-# PER_ITERATION = 100
-# KEEP = 20
-# EXPORT_TOP = 1
-# EXPORT_THRESHOLD = 430
-# PROB = 0.01
-# EDGE_PROB = 0.5
-
-# osc = False
-# more_edges = False
-# run_type = 'normal'
-
-# DEFAULT_SOURCES = {'ABA':0}
-
-
-
-# # BASE = 'networkmutation/baseline/ABA_full_20230407.txt'
-# # BASE = 'networkmutation/baseline/ABA_full_fix_20230407.txt'
-# BASE = 'networkmutation/baseline/ABA_GA_base_A_20230501.txt'
-# # BASE = 'networkmutation/baseline/ABA_GA_base_B_20230407.txt'
-
-# MODEL = BASE
-# ### GA1
-# # MODEL = 'networkmutation/models/20230512_7790_gen125_mod.txt'
-# ### GA2
-# # MODEL = 'networkmutation/models/osc_20230514_8025_gen128.txt'
-
-
-
-
-# DATA = 'networkmutation/data/data_20230426.tsv'
-
-# ### constraints for the typical run
-# CONSTRAINTS = {'fixed': {'Ca2_ATPase', 'Ca2c', 'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
-#                 'regulate': {'ABI1':('RCARs',), 'ABI2':('RCARs',), 'HAB1':('RCARs',), 'PP2CA':('RCARs',), 'K_efflux':('KEV','KOUT'), 'OST1':('ABI1','ABI2'), 'Depolarization':('AnionEM',)},
-#                 'necessary' : {'8-nitro-cGMP':('cGMP',), 'KOUT':('Depolarization',), 'Malate':('PEPC', 'AnionEM'), 'ROS':('NADPH', 'RBOH')},
-#                 'group': {'PA':(('PC','PLDalpha'),('PC','PLDdelta'),('DAG','DAGK')), 'S1P_PhytoS1P':(('SPHK1_2','Sph'),)},
-#                 'possible_constant': set()}
-# # ### constraints for the 'more edges' version 20230524 - allow change of Ca2c, move to regulate
-# # CONSTRAINTS = {'fixed': {'Ca2_ATPase', 'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
-# #                'regulate': {'ABI1':('RCARs',), 'ABI2':('RCARs',), 'HAB1':('RCARs',), 'PP2CA':('RCARs',), 'K_efflux':('KEV','KOUT'), 'OST1':('ABI1','ABI2'), 'Depolarization':('AnionEM',), 'Ca2c':('CaIM','CIS','Ca2_ATPase')},
-# #                'necessary' : {'8-nitro-cGMP':('cGMP',), 'KOUT':('Depolarization',), 'Malate':('PEPC', 'AnionEM'), 'ROS':('NADPH', 'RBOH')},
-# #                'group': {'PA':(('PC','PLDalpha'),('PC','PLDdelta'),('DAG','DAGK')), 'S1P_PhytoS1P':(('SPHK1_2','Sph'),)},
-# #                'possible_constant': set()}
-
-# ### edge pool for the typical run 20230801 - 13 edges
-# EDGE_POOL = [('Ca2c', 'ABI2', '0'),('Ca2c', 'HAB1', '0'),('Ca2c', 'PP2CA', '0'),
-#                 ('PA', 'ABI2', '0'),('PA', 'HAB1', '0'),('PA', 'PP2CA', '0'),
-#                 ('AquaporinPIP2_1', 'ROS', '1'),
-#                 ('Actin_Reorganization', 'RBOH', '1'), ('ROS', 'Actin_Reorganization', '1'),
-#                 ('pHc', 'Vacuolar_Acidification', '1'), ('ABI1', 'GEF1_4_10', '1'),
-#                 ('GHR1', 'CPK3_21', '1'),
-#                 ('PA', 'Microtubule_Depolymerization', '1')]
-# # ### more edges version 20230704 - 14 edges
-# # EDGE_POOL = (('PA', 'ABI2', '0'),
-# #              ('AquaporinPIP2_1', 'ROS', '1'),
-# #              ('Actin_Reorganization', 'RBOH', '1'),
-# #              ('ROS', 'Actin_Reorganization', '1'),
-# #              ('pHc', 'Vacuolar_Acidification', '1'),
-# #              ('PA', 'Microtubule_Depolymerization', '1'),
-# #              ('GHR1', 'KOUT', '1'),
-# #              ('NO', 'KEV', '1'),
-# #              ('CIS', 'AnionEM', '1'),
-# #              ('GPA1', 'Ca2c', '1'), ('PA', 'Ca2c', '1'),
-# #              ('pHc', 'SPHK1_2', '1'),
-# #              ('InsP3', 'SPHK1_2', '1'),
-# #              ('ABA', 'GEF1_4_10', '0'))
-
-# if run_type == 'osc':
-#     DATA = 'networkmutation/data/data_osc_20230426.tsv'
-
-#     ### constraints for the typical run for ca_osc model
-#     CONSTRAINTS = {'fixed': {'Ca2osc', 'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
-#                    'regulate': {'ABI1':('RCARs',), 'ABI2':('RCARs',), 'HAB1':('RCARs',), 'PP2CA':('RCARs',), 'K_efflux':('KEV','KOUT'), 'OST1':('ABI1','ABI2'), 'Depolarization':('AnionEM',)},
-#                    'necessary' : {'8-nitro-cGMP':('cGMP',), 'KOUT':('Depolarization',), 'Malate':('PEPC', 'AnionEM'), 'ROS':('NADPH', 'RBOH')},
-#                    'group': {'PA':(('PC','PLDalpha'),('PC','PLDdelta'),('DAG','DAGK')), 'S1P_PhytoS1P':(('SPHK1_2','Sph'),)},
-#                    'possible_constant': set()}
-#     # ### constraints for the 'more edges' version - allow modification of Ca2osc, move to regulate
-#     # CONSTRAINTS = {'fixed': {'Closure', 'DAG', 'H2O_Efflux', 'InsP3', 'InsP6', 'NO', 'PtdIns3_5P2', 'PtdIns4_5P2', 'RCARs', 'cADPR', 'cGMP'},
-#     #                'regulate': {'ABI1':('RCARs',), 'ABI2':('RCARs',), 'HAB1':('RCARs',), 'PP2CA':('RCARs',), 'K_efflux':('KEV','KOUT'), 'OST1':('ABI1','ABI2'), 'Depolarization':('AnionEM',), 'Ca2osc':('CaIM','CIS')},
-#     #                'necessary' : {'8-nitro-cGMP':('cGMP',), 'KOUT':('Depolarization',), 'Malate':('PEPC', 'AnionEM'), 'ROS':('NADPH', 'RBOH')},
-#     #                'group': {'PA':(('PC','PLDalpha'),('PC','PLDdelta'),('DAG','DAGK')), 'S1P_PhytoS1P':(('SPHK1_2','Sph'),)},
-#     #                'possible_constant': set()}
-
-#     ### edge pool for the typical run for the ca_osc model 20230801 - 13 edges
-#     EDGE_POOL = [('Ca2osc', 'ABI2', '0'),('Ca2osc', 'HAB1', '0'),('Ca2osc', 'PP2CA', '0'),
-#                  ('PA', 'ABI2', '0'),('PA', 'HAB1', '0'),('PA', 'PP2CA', '0'),
-#                  ('AquaporinPIP2_1', 'ROS', '1'),
-#                  ('Actin_Reorganization', 'RBOH', '1'), ('ROS', 'Actin_Reorganization', '1'),
-#                  ('pHc', 'Vacuolar_Acidification', '1'), ('ABI1', 'GEF1_4_10', '1'),
-#                  ('GHR1', 'CPK3_21', '1'),
-#                  ('PA', 'Microtubule_Depolymerization', '1')]
-#     # ### more edges version 20230704 - 14 edges
-#     # EDGE_POOL = (('PA', 'ABI2', '0'),
-#     #              ('AquaporinPIP2_1', 'ROS', '1'),
-#     #              ('Actin_Reorganization', 'RBOH', '1'),
-#     #              ('ROS', 'Actin_Reorganization', '1'),
-#     #              ('pHc', 'Vacuolar_Acidification', '1'),
-#     #              ('PA', 'Microtubule_Depolymerization', '1'),
-#     #              ('GHR1', 'KOUT', '1'),
-#     #              ('NO', 'KEV', '1'),
-#     #              ('CIS', 'AnionEM', '1'),
-#     #              ('GPA1', 'Ca2osc', '1'), ('PA', 'Ca2osc', '1'),
-#     #              ('pHc', 'SPHK1_2', '1'),
-#     #              ('InsP3', 'SPHK1_2', '1'),
-#     #              ('ABA', 'GEF1_4_10', '0'))
-
-
-
-
 
 id = 0
--- boolmore/detailed_report.py
+++ boolmore/detailed_report.py
@@ -48,109 +48,109 @@ if __name__ == '__main__':
     print('default_sources:', n1.default_sources)
     print()
 
-    # ### percolate the constants and reduce the model
-    # pprimes1 = pi.percolate(primes, remove_constants = True, copy=True)
-    # print('percolated rules: ')
-    # sm.format.pretty_print_prime_rules({k:pprimes1[k] for k in sorted(pprimes1)}) # type: ignore
-    # print()
+    ### percolate the constants and reduce the model
+    pprimes1 = pi.percolate(primes, remove_constants = True, copy=True)
+    print('percolated rules: ')
+    sm.format.pretty_print_prime_rules({k:pprimes1[k] for k in sorted(pprimes1)}) # type: ignore
+    print()
 
-    # ### find nodes that have different functions
-    # pprimes0 = pi.percolate(base_primes, remove_constants = True, copy=True)
-    # different_nodes = []
-    # for node in pprimes0: # type: ignore
-    #     if node not in pprimes1:
-    #         different_nodes.append(node)
-    #         continue
-    #     if pprimes0[node] == pprimes1[node]: # type: ignore
-    #         pass
-    #     else:
-    #         different_nodes.append(node)
-    # for node in pprimes1: # type: ignore
-    #     if node not in pprimes0:
-    #         different_nodes.append(node)
-    # print('Nodes with changed percolated functions: ', sorted(different_nodes))
-    # print()
+    ### find nodes that have different functions
+    pprimes0 = pi.percolate(base_primes, remove_constants = True, copy=True)
+    different_nodes = []
+    for node in pprimes0: # type: ignore
+        if node not in pprimes1:
+            different_nodes.append(node)
+            continue
+        if pprimes0[node] == pprimes1[node]: # type: ignore
+            pass
+        else:
+            different_nodes.append(node)
+    for node in pprimes1: # type: ignore
+        if node not in pprimes0:
+            different_nodes.append(node)
+    print('Nodes with changed percolated functions: ', sorted(different_nodes))
+    print()
 
-    # ### find the edges that are deleted
-    # deleted_regulators = {}
-    # deleted_regulators_perc = {}
-    # for node in base_primes:
-    #     base_regulators = pi.find_predecessors(base_primes, [node])
-    #     regulators = pi.find_predecessors(primes, [node])
-    #     deleted_regulator_list = [x for x in base_regulators if x not in regulators]
-    #     if len(deleted_regulator_list) != 0:
-    #         deleted_regulators[node] = deleted_regulator_list
-    #     ### find extra edges that are deleted in the percolated rules
-    #     if node in pprimes0 and node in pprimes1:
-    #         base_regulators_perc = pi.find_predecessors(pprimes0, [node]) # type: ignore
-    #         regulators_perc = pi.find_predecessors(pprimes1, [node]) # type: ignore
-    #         deleted_regulator_list_perc = [x for x in base_regulators_perc if x not in regulators_perc and x not in deleted_regulator_list]
-    #         if len(deleted_regulator_list_perc) != 0:
-    #             deleted_regulators_perc[node] = deleted_regulator_list_perc
-    # print('deleted regulators: ',deleted_regulators)
-    # print('extra deleted regulators in the percolated rules: ',deleted_regulators_perc)
-    # print()
+    ### find the edges that are deleted
+    deleted_regulators = {}
+    deleted_regulators_perc = {}
+    for node in base_primes:
+        base_regulators = pi.find_predecessors(base_primes, [node])
+        regulators = pi.find_predecessors(primes, [node])
+        deleted_regulator_list = [x for x in base_regulators if x not in regulators]
+        if len(deleted_regulator_list) != 0:
+            deleted_regulators[node] = deleted_regulator_list
+        ### find extra edges that are deleted in the percolated rules
+        if node in pprimes0 and node in pprimes1:
+            base_regulators_perc = pi.find_predecessors(pprimes0, [node]) # type: ignore
+            regulators_perc = pi.find_predecessors(pprimes1, [node]) # type: ignore
+            deleted_regulator_list_perc = [x for x in base_regulators_perc if x not in regulators_perc and x not in deleted_regulator_list]
+            if len(deleted_regulator_list_perc) != 0:
+                deleted_regulators_perc[node] = deleted_regulator_list_perc
+    print('deleted regulators: ',deleted_regulators)
+    print('extra deleted regulators in the percolated rules: ',deleted_regulators_perc)
+    print()
 
-    # ### find stable motifs
-    # ### TODO: not include the source nodes.
-    # stable_motifs = ts.compute_trap_spaces(pprimes1, "max") # type: ignore
-    # print("Stable Motifs:")
-    # for stable_motif in stable_motifs:
-    #     print(stable_motif)
-    # print()
+    ### find stable motifs
+    ### TODO: not include the source nodes.
+    stable_motifs = ts.compute_trap_spaces(pprimes1, "max") # type: ignore
+    print("Stable Motifs:")
+    for stable_motif in stable_motifs:
+        print(stable_motif)
+    print()
 
-    # ### fix extra nodes to find conditionally stable motifs
-    # ### WARNING: fixing constant nodes do not work, as they are already percolated
-    # print("Conditionally Stable Motifs:")
-    # fixes = [{'ABA': 0}, {'ABA': 1}]
-    # for fix in fixes:
-    #     print('fix - ', fix)
-    #     pprimes2 = pi.percolate(pprimes1, add_constants = fix, remove_constants = True, copy=True) # type: ignore
-    #     # sm.format.pretty_print_prime_rules({k:primes2[k] for k in sorted(primes2)})
-    #     conditinally_stable_motifs = ts.compute_trap_spaces(pprimes2, "max") # type: ignore
-    #     for conditinally_stable_motif in conditinally_stable_motifs:
-    #         if conditinally_stable_motif not in stable_motifs:
-    #             print(conditinally_stable_motif)
-    # print()
+    ### fix extra nodes to find conditionally stable motifs
+    ### WARNING: fixing constant nodes do not work, as they are already percolated
+    print("Conditionally Stable Motifs:")
+    fixes = [{'ABA': 0}, {'ABA': 1}]
+    for fix in fixes:
+        print('fix - ', fix)
+        pprimes2 = pi.percolate(pprimes1, add_constants = fix, remove_constants = True, copy=True) # type: ignore
+        # sm.format.pretty_print_prime_rules({k:primes2[k] for k in sorted(primes2)})
+        conditinally_stable_motifs = ts.compute_trap_spaces(pprimes2, "max") # type: ignore
+        for conditinally_stable_motif in conditinally_stable_motifs:
+            if conditinally_stable_motif not in stable_motifs:
+                print(conditinally_stable_motif)
+    print()
 
-    # ### fix nodes to find minimal trapspaces and LDOIs
-    # ### fixing constant nodes are not allowed
-    # print("LDOI, DOI and minimal trapspaces:")
-    # fixes = [{'ABA':0},
-    #          {'ABA':1},
-    #          {'ROS':1},
-    #          {'NO':1},
-    #          {'CaIM':1},
-    #          {'cADPR':1},
-    #          {'PA':1},
-    #          {'pHc':1},
-    #          {'S1P_PhytoS1P':1},
-    #          {'AtRAC1':0},
-    #          {'InsP3':1}]
-    # for fix in fixes:
-    #     print("- - - - - - - - - -")
-    #     print("fix -", fix)
-    #     LDOI = sm.drivers.logical_domain_of_influence(fix,pprimes1)[0]
-    #     MPBN_DOI = sm.drivers.domain_of_influence(fix,pprimes1,MPBN_update=True)[0] # type: ignore
+    ### fix nodes to find minimal trapspaces and LDOIs
+    ### fixing constant nodes are not allowed
+    print("LDOI, DOI and minimal trapspaces:")
+    fixes = [{'ABA':0},
+             {'ABA':1},
+             {'ROS':1},
+             {'NO':1},
+             {'CaIM':1},
+             {'cADPR':1},
+             {'PA':1},
+             {'pHc':1},
+             {'S1P_PhytoS1P':1},
+             {'AtRAC1':0},
+             {'InsP3':1}]
+    for fix in fixes:
+        print("- - - - - - - - - -")
+        print("fix -", fix)
+        LDOI = sm.drivers.logical_domain_of_influence(fix,pprimes1)[0]
+        MPBN_DOI = sm.drivers.domain_of_influence(fix,pprimes1,MPBN_update=True)[0] # type: ignore
 
-    #     print('LDOI: ',{k:LDOI[k] for k in sorted(LDOI)})
-    #     print('only in MPBN_DOI: ',{k:MPBN_DOI[k] for k in MPBN_DOI if k not in LDOI})
-    #     print()
+        print('LDOI: ',{k:LDOI[k] for k in sorted(LDOI)})
+        print('only in MPBN_DOI: ',{k:MPBN_DOI[k] for k in MPBN_DOI if k not in LDOI})
+        print()
 
-    #     if 'ABA' not in fix:
-    #         fix.update({'ABA':0})
+        if 'ABA' not in fix:
+            fix.update({'ABA':0})
         
-    #     pprimes2 = pi.percolate(pprimes1, add_constants = fix, remove_constants = False, copy=True) # type: ignore
-    #     trs = ts.compute_trap_spaces(pprimes2, "min") # type: ignore
-    #     for tr in trs:
-    #         for node in pprimes1.keys(): # type: ignore
-    #             if node not in tr.keys(): # type: ignore
-    #                 tr[node] = 'X' # type: ignore
+        pprimes2 = pi.percolate(pprimes1, add_constants = fix, remove_constants = False, copy=True) # type: ignore
+        trs = ts.compute_trap_spaces(pprimes2, "min") # type: ignore
+        for tr in trs:
+            for node in pprimes1.keys(): # type: ignore
+                if node not in tr.keys(): # type: ignore
+                    tr[node] = 'X' # type: ignore
         
-    #     if len(trs) == 1:
-    #         print("There is 1 minimal trapspace for ", dict(sorted(fix.items())))
-    #     else:
-    #         print("There are " + str(len(trs)) + " minimal trapspaces for", dict(sorted(fix.items())))
-    #     for tr in trs:
-    #         print(dict(sorted(tr.items()))) # type: ignore
-    #     print()
+        if len(trs) == 1:
+            print("There is 1 minimal trapspace for ", dict(sorted(fix.items())))
+        else:
+            print("There are " + str(len(trs)) + " minimal trapspaces for", dict(sorted(fix.items())))
+        for tr in trs:
+            print(dict(sorted(tr.items()))) # type: ignore
+        print()
