40eebd8c339978f30ea64503d8d85540f8932ed5
Kyu Hyong Park <kjp5774@psu.edu>
2024-07-26 14:55:02 -0400

    add seed to data generation

--- optimization/data_generation_sample.py
+++ optimization/data_generation_sample.py
@@ -9,7 +9,11 @@ from boolmore.benchmarks import generate_experiments
 
 base_directory = "boolmore/benchmarks/benchmark_models"
 
-sample_models = ["IL-1 Signaling",
+sample_models = ["Cell Cycle Transcription by Coupled CDK and Network Oscillators",
+                 "Metabolic Interactions in the Gut Microbiome",
+                 "Mammalian Cell Cycle 2006",
+                 "T-LGL Survival Network 2011 Reduced Network",
+                 "IL-1 Signaling",
                  "Glucose Repression Signaling 2009",
                  "Signaling in Macrophage Activation",
                  "Influenza A Virus Replication Cycle"]
@@ -20,11 +24,12 @@ data_directory = "boolmore/optimization/data"
 for model_name in sample_models:
     base_primes = sm.format.import_primes(base_directory + "/" + model_name + ".txt")
     for i in range(5):
-        exps, fixes_list = generate_experiments(base_primes, export=True, file_name="boolmore/optimization/data/"+model_name+"_data_"+str(i+1)+".tsv")
+        exps, fixes_list = generate_experiments(base_primes, seed = i,
+                                                export=True, file_name="boolmore/optimization/data/"+model_name+"_data_"+str(i+1)+".tsv")
         
         base = Model.import_model(base_primes)
         
-        start = base.mutate(0.5, 0)
+        start = base.mutate(0.5, 0, seed=i)
 
         start.get_predictions(fixes_list)
         start.get_model_score(exps)
