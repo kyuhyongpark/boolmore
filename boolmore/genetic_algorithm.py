a3cf13b10df6b0447bb493c9cf2ed4e75becfead
Kyu Hyong Park <kjp5774@psu.edu>
2024-09-06 13:13:05 -0400

    choose without replacement when mixing

--- boolmore/genetic_algorithm.py
+++ boolmore/genetic_algorithm.py
@@ -3,6 +3,7 @@ import random
 import pystablemotifs as sm
 import boolmore.config
 import time
+import numpy as np
 
 from boolmore.model import Model, mix_models
 from boolmore.experiment import import_exps
@@ -270,6 +271,7 @@ def ga_main(start:Model, exps:list[ExpType], fixes_list:list[FixesType],
 
     if seed != None:
         random.seed(seed)
+        np.random.seed(seed)
 
     log = []
     iteration = []
@@ -331,7 +333,7 @@ def ga_main(start:Model, exps:list[ExpType], fixes_list:list[FixesType],
 
         mixed_model_lst = []
         for j in range(mix):
-            model_choice = random.choices(to_be_mixed, weights=weights, k=2)
+            model_choice = np.random.choice(to_be_mixed, size = 2, replace = False, p=weights)
             mixed_model = mix_models(model_choice[0], model_choice[1])
             mixed_model_lst.append(mixed_model)    
         if core > 1:
