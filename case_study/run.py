63581ddf9bb0e2f6ae7ecd912401c10fb23f8968
Kyu Hyong Park <kjp5774@psu.edu>
2024-10-24 12:40:54 -0400

    allow flexible run

--- case_study/run.py
+++ case_study/run.py
@@ -4,6 +4,14 @@ from boolmore.genetic_algorithm import run_ga
 
 JSON = "./data/ABA_2017.json"
 START_MODEL = "./baseline_models/ABA_GA_base_A.txt"
-RUN_NAME = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-run_ga(JSON, START_MODEL, RUN_NAME, core=6, seed=0)
+overwrite_parameters = {
+    "total_iterations" : 100,
+    "per_iteration" : 100,
+    "keep" : 20,
+    "mix" : 0,
+    }
+for i in range(3, 30):
+    RUN_NAME = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    run_ga(JSON, START_MODEL, RUN_NAME, parameter_dict=overwrite_parameters, core=6, seed=i)
