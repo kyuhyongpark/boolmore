412a2a70acbb024547714c047f62973d2cc772ae
Kyu Hyong Park <kjp5774@psu.edu>
2024-11-07 14:15:05 -0500

    clean up demo

--- demo/README.md
+++ demo/README.md
@@ -13,4 +13,4 @@ models per iteration: 10 (compared to 100 for benchmarks)
 models to keep per iteration: 2 (compared to 20 for benchmarks)
 
 the log will be stored as "demo_log.txt"
-the final model will be stored as "demo_id_gen.txt"
+the final model will be stored as "demo_id_gen.bnet"
--- demo/run.py
+++ demo/run.py
@@ -1,9 +1,17 @@
-import boolmore.genetic_algorithm
-from boolmore.conversions import prime2bnet
+from boolmore.genetic_algorithm import run_ga
+from boolmore.conversions import prime2bnet, prime2rr
 
-base, start, final = boolmore.genetic_algorithm.run_ga("CAD_config.json", "CAD_start.txt",
-                                                       run_name="demo", stop_if_max=True)
+base, start, final = run_ga("CAD_config.json",
+                            "CAD_start.bnet",
+                            run_name="demo",
+                            stop_if_max=True)
 
+print("\n-----comparing with the baseline functions-----")
+modified = 0
 for node in base.primes:
-    print("base:" + prime2bnet(node, base.primes[node])) # type: ignore
-    print("final:" + prime2bnet(node, final.primes[node])) # type: ignore
+    if prime2rr(base.primes[node])[1] != prime2rr(final.primes[node])[1]:
+        modified += 1
+        print("base:" + prime2bnet(node, base.primes[node])) # type: ignore
+        print("final:" + prime2bnet(node, final.primes[node])) # type: ignore
+
+print(f"\n{modified} out of {len(base.primes)} functions differ from the baseline")
