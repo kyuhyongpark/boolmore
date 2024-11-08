8b1937696c82dc87b9b57855d6e5998e0fe25d88
Kyu Hyong Park <kjp5774@psu.edu>
2024-11-08 12:36:26 -0500

    reorganize benchmarks directory

---
+++ Benchmarks/optimization/scripts/data_generation_sample.py
@@ -0,0 +1,37 @@
+# generate data for optimization
+# only the data for the large models are generated.
+# for the small models, use data pre-generated for the benchmark.
+
+import pystablemotifs as sm
+
+from boolmore.model import Model
+from boolmore.benchmarks import generate_experiments
+
+base_directory = "boolmore/benchmarks/benchmark_models"
+
+sample_models = ["Cell Cycle Transcription by Coupled CDK and Network Oscillators",
+                 "Metabolic Interactions in the Gut Microbiome",
+                 "Mammalian Cell Cycle 2006",
+                 "T-LGL Survival Network 2011 Reduced Network",
+                 "IL-1 Signaling",
+                 "Glucose Repression Signaling 2009",
+                 "Signaling in Macrophage Activation",
+                 "Influenza A Virus Replication Cycle"]
+
+data_directory = "boolmore/optimization/data"
+
+### Generate data
+for model_name in sample_models:
+    base_primes = sm.format.import_primes(base_directory + "/" + model_name + ".txt")
+    for i in range(5):
+        exps, fixes_list = generate_experiments(base_primes, seed = i,
+                                                export=True, file_name="boolmore/optimization/data/"+model_name+"_data_"+str(i+1)+".tsv")
+        
+        base = Model.import_model(base_primes)
+        
+        start = base.mutate(0.5, 0, seed=i)
+
+        start.get_predictions(fixes_list)
+        start.get_model_score(exps)
+        
+        start.export("boolmore/optimization/data/"+model_name+"_start_"+str(i+1)+".txt")
