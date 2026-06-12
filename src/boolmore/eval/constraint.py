0328e7918f16610e1a8df192904e85c33c8fed8e
kyuhyongpark <kjp5774@psu.edu>
2026-06-12 09:28:18 +0100

    refactor: make constraint check collect all errors

--- src/boolmore/eval/constraint.py
+++ src/boolmore/eval/constraint.py
@@ -17,14 +17,15 @@ def check_constant(rr:str) -> bool:
     check - True if the rule is constant    :bool
 
     """
-    max_rr = conv.get_uni_rr(rr, max = True)
-    if '0' in max_rr and '1' in max_rr:
-        return False
-    else:
-        return True
+    max_rr = conv.get_uni_rr(rr, max=True)
+    return not ('0' in max_rr and '1' in max_rr)
 
 
-def check_source(regulators:tuple[str, ...], rr:str, node:str) -> bool:
+def check_source(
+    regulators: tuple[str, ...],
+    rr: str,
+    node: str,
+) -> bool:
     """
     Check if the node became a source node.
 
@@ -39,23 +40,27 @@ def check_source(regulators:tuple[str, ...], rr:str, node:str) -> bool:
     check - True if the node became a source    :bool
     
     """
-    # k = number of regulators
+    if node not in regulators:
+        return False
+
     k = len(regulators)
-    # n = nth regulator
     n = regulators.index(node)
 
-    min_rr = conv.get_uni_rr(rr, max = False)
+    min_rr = conv.get_uni_rr(rr, max=False)
+
     add = 0
     for i in range(2**k):
         add += int(min_rr[i])
 
-    if add == 1 and min_rr[2**k-2**(k-n-1)-1] == '1':
-        return True
-    else:
-        return False
+    return add == 1 and min_rr[2**k - 2**(k - n - 1) - 1] == '1'
 
 
-def check_regulate(regulators:tuple[str, ...], rr:str, reg:str, is_min:bool=False) -> bool:
+def check_regulate(
+    regulators: tuple[str, ...],
+    rr: str,
+    reg: str,
+    is_min: bool = False,
+) -> bool:
     """
     Checks if a regulator appears in the rule in a non-redundant manner.
 
@@ -69,36 +74,28 @@ def check_regulate(regulators:tuple[str, ...], rr:str, reg:str, is_min:bool=Fals
     Returns
     -------
     check - True if the regulator appears   :bool
-
     """
+    if reg not in regulators:
+        return False
 
-    assert reg in regulators, "The node should be one of the regulators"
-
-    check = False
-
-    # k = number of regulators
     k = len(regulators)
-    # n = nth regulator
     n = regulators.index(reg)
 
-    # bi = the minimal representation.
-    if is_min:
-        bi = rr
-    else:
-        bi = conv.get_uni_rr(rr, max = False)
+    bi = rr if is_min else conv.get_uni_rr(rr, max=False)
 
     add = 0
     for i in range(2**k):
-        if i % 2**(k-n) <= 2**(k-1-n)-1:
+        if i % 2**(k - n) <= 2**(k - 1 - n) - 1:
             add += int(bi[i])
 
-    if add != 0:
-        check = True
-
-    return check
+    return add != 0
 
 
-def check_necessary(regulators:tuple[str, ...], rr:str, nec:str) -> bool:
+def check_necessary(
+    regulators: tuple[str, ...],
+    rr: str,
+    nec: str,
+) -> bool:
     """
     Checks if the node is necessary for the rule.
 
@@ -113,29 +110,30 @@ def check_necessary(regulators:tuple[str, ...], rr:str, nec:str) -> bool:
     check - True if the regulator appears   :bool
 
     """
-    assert nec in regulators, "The node should be one of the regulators"
-
-    check = False
-
-    # k = number of regulators
-    k = len(regulators)
-    # n = nth regulator
-    n = regulators.index(nec)
-
-    if check_regulate(regulators, rr, nec) == False:
+    if nec not in regulators:
         return False
+
+    if not check_regulate(regulators, rr, nec):
+        return False
+
+    k = len(regulators)
+    n = regulators.index(nec)
 
     add = 0
     for i in range(2**k):
-        if i % 2**(k-n) > 2**(k-1-n)-1:
+        if i % 2**(k - n) > 2**(k - 1 - n) - 1:
             add += int(rr[i])
 
-    if add == 0:
-        check = True
-    return check
+    return add == 0
 
 
-def check_node(regulators:tuple[str, ...], rr:str, base_rr:str, constraints:dict, node:str) -> bool:
+def check_node(
+    regulators: tuple[str, ...],
+    rr: str,
+    base_rr: str,
+    constraints: dict,
+    node: str,
+) -> bool:
     """
     Checks if the model follows fixed, regulate, necessary, possible_constant constraints.
     Also checks if a non-source became a source, or if a non-constant became a constant.
@@ -154,69 +152,79 @@ def check_node(regulators:tuple[str, ...], rr:str, base_rr:str, constraints:dict
 
     Returns
     -------
-    check - True if follows constraints     :bool
-    
+    bool
+        True if all checks pass.
     """
 
+    errors: list[str] = []
+
     # a constant node should not be mutated
-    if len(regulators) == 0:
-        if rr == base_rr:
-            return True
-        else:
-            print("Constant node", node, "was mutated")
-            return False
+    if len(base_rr) == 1:
+        if rr != base_rr:
+            errors.append(f"Constant node {node} was mutated")
 
     # a source node should not be mutated
-    if len(regulators) == 1 and regulators[0] == node:
-        if rr == base_rr:
-            return True
-        else:
-            print("Source node", node, "was mutated")
-            return False
+    elif len(base_rr) == 2 and regulators[0] == node:
+        if rr != base_rr:
+            errors.append(f"Source node {node} was mutated")
 
-    # check constraint - fixed
-    if node in constraints['fixed']:
-        if rr == base_rr:
-            return True
-        else:    
-            print(node, "with fixed function was mutated")
-            return False
+    # fixed function
+    elif node in constraints["fixed"]:
+        if rr != base_rr:
+            errors.append(f"{node} with fixed function was mutated")
 
-    if node in constraints['necessary']:
-        for necc in constraints['necessary'][node]:
-            if not check_necessary(regulators, rr, necc):
-                print(necc, "should be necessary for", node)
-                return False
+    else:
+        # necessary constraints
+        if node in constraints["necessary"]:
+            for necc in constraints["necessary"][node]:
+                if necc not in regulators:
+                    errors.append(
+                        f"{node}: necessary regulator {necc} is completely absent"
+                    )
+                elif not check_necessary(regulators, rr, necc):
+                    errors.append(
+                        f"{necc} should be necessary for {node}"
+                    )
 
-    if node in constraints['regulate']:
-        for reg in constraints['regulate'][node]:
-            if not check_regulate(regulators, rr, reg):
-                print(reg, "should regulate", node)
-                return False
+        # regulate constraints
+        if node in constraints["regulate"]:
+            for reg in constraints["regulate"][node]:
+                if reg not in regulators:
+                    errors.append(
+                        f"{node}: regulator {reg} is completely absent"
+                    )
+                elif not check_regulate(regulators, rr, reg):
+                    errors.append(
+                        f"{reg} should regulate {node}"
+                    )
 
-    # node with a self loop should not become a source node
-    if node in regulators:
-        if check_source(regulators, rr, node):
-            print(node, "should not be a source")
-            return False
+        # node with a self loop should not become a source node
+        if node in regulators:
+            if check_source(regulators, rr, node):
+                errors.append(f"{node} should not be a source")
 
-    # only nodes that were originally a constant node
-    # or nodes in possible_constant can become constants
-    if check_constant(rr):
-        max_rr = conv.get_uni_rr(rr, max = True)
-        if node not in constraints['possible_constant'] and len(base_rr) != 1:
-            print(node, "should not be a constant")
-            return False
-        # a constant node should not have its value changed
-        elif base_rr == '1' and '0' in max_rr:
-            print(node, "has opposite constant value")
-            return False
-        elif base_rr == '0' and '1' in max_rr:
-            print(node, "has opposite constant value")
-            return False
+        # constant checks
+        if check_constant(rr):
+            max_rr = conv.get_uni_rr(rr, max=True)
+
+            if (
+                node not in constraints["possible_constant"]
+                and len(base_rr) != 1
+            ):
+                errors.append(f"{node} should not be a constant")
+
+            elif base_rr == "1" and "0" in max_rr:
+                errors.append(f"{node} has opposite constant value")
+
+            elif base_rr == "0" and "1" in max_rr:
+                errors.append(f"{node} has opposite constant value")
+
+    if errors:
+        for error in errors:
+            print(error)
+        return False
 
     return True
-
 
 
 
